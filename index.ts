4bfbd1e498aeb166f38bf61ea5345d87d136765a
Pankaj <pankajkumarhzb787@gmail.com>
2025-11-09 14:32:30 +0530

    setup llm

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import { ChatGroq } from "@langchain/groq";
+
+const tools: any = [];
+
+const model = new ChatGroq({
+    model: "openai/gpt-oss-120b",
+    temperature: 0
+}).bindTools(tools);
