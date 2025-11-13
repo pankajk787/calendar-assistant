26cafc50c59e98379f9d69100531e5021d6fb15a
Pankaj <pankajkumarhzb787@gmail.com>
2025-11-14 00:11:50 +0530

    implement tools

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import { ChatGroq } from "@langchain/groq";
 import { createEventTool, getEventsTool } from "./tools";
+import { END, MessagesAnnotation, StateGraph } from "@langchain/langgraph";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import type { AIMessage } from "@langchain/core/messages";
 
 const tools: any = [createEventTool, getEventsTool];
 
@@ -7,3 +10,56 @@ const model = new ChatGroq({
     model: "openai/gpt-oss-120b",
     temperature: 0
 }).bindTools(tools);
+
+
+/**
+ * Assisstant node
+ */
+async function callModel(state: typeof MessagesAnnotation.State) {
+    const response = await model.invoke(state.messages);
+    return { messages: [response] };
+}
+
+/**
+ * Tool node
+ */
+const toolNode = new ToolNode(tools);
+
+/**
+ * Build the graph
+ */
+
+function shouldContinueTo (state: typeof MessagesAnnotation.State) {
+    const lastMessage = state.messages[state.messages.length - 1] as AIMessage;
+    if(lastMessage.tool_calls?.length) {
+        return "tool_calls";
+    }
+    return "__end__";
+}
+
+const graph = new StateGraph(MessagesAnnotation)
+    .addNode("assistant", callModel)
+    .addNode("tool_calls", toolNode)
+    .addEdge("__start__", 'assistant')
+    .addEdge("tool_calls", "assistant")
+    .addConditionalEdges("assistant", shouldContinueTo, {
+        "__end__" : END,
+        tool_calls : "tool_calls"
+    }
+);
+
+const app = graph.compile();
+
+async function main() {
+    const config = { configurable: { thread_id: '1' } };
+    const result = await app.invoke({
+        messages: [
+            { role: "human", content: "Create a meeting with Pankaj @10:30 AM tomorrow."},
+            { role: "system", content: `You are a personal assisstant. Current date and time ${new Date().toUTCString()}.` }
+        ]
+    }, config);
+
+    const finalMessage = result.messages[result.messages.length - 1];
+    console.log("AI: ", finalMessage?.content);
+}
+main();
--- tools.ts
+++ tools.ts
@@ -1,29 +1,25 @@
 import { tool } from "@langchain/core/tools";
 import * as z from "zod";
 
-export const createEventTool = tool(async () => {
-        return JSON.stringify([{
-            title: "Meeting with Pankaj",
-            date: "11th Nov 2025",
-            time: "2",
-            location: "Gmeet"
-        }])
+export const createEventTool = tool(async ( { query }) => {
+        return "Meeting has been created."
     },
     {
         name: "create-event",
         description: "call to create the calendar event.",
-        schema: z.object({})
+        schema: z.object({ query: z.string().describe("The query to be used to create event into Google Calendar.") })
     }
 )
 
-export const getEventsTool = tool(async () =>{
+export const getEventsTool = tool(async ({ query }) =>{
     return JSON.stringify([{
         title: "Meeting with Pankaj",
-        date: "11th Nov 2025",
+        date: "15th Nov 2025",
         time: "2",
         location: "Gmeet"
     }])
 }, {
     name: 'get-events',
     description: "Call to get the calendar events.",
+    schema: z.object({ query: z.string().describe("The query to be used to describe get the vents from google calendar.")})
 })
